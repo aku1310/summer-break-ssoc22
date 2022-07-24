0928d813f1dcdfe7fad2c589734e1cf9634d1983
Vishrut Aggarwal <vishrut.agg@gmail.com>
2022-07-24 20:15:50 +0530

    Feature: Added Sponsors section

--- src/components/Sponsor.js
+++ src/components/Sponsor.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-let sponsorList = {
+let sponsorList = [{
   platinum: [
     {
       name: "Devfolio",
@@ -10,21 +10,40 @@ let sponsorList = {
       name: "Polygon",
       url: "#"
     }
-  ],
+  ]},
+  {
   gold: [
     {
       name: "FileCoin",
       url: "#"
     }
-  ]
-}
+  ]}
+]
 
 function Sponsor() {
   return (
     <div id='sponsor-page' className='page'>
         <h1>Sponsors</h1>
         <div className='sponsor-card-container'>
+          <h4>Platinum Sponsors</h4>
+          <section id='platinum-sponsors'>
+            <div className='card'>
+              <h6>Devfolio</h6>
+              <img src='#' alt='devfolio-logo'/>
+            </div>
+            <div className='card'>
+              <h6>Polygon</h6>
+              <img src='#' alt='polygon-logo'/>
+            </div>
+          </section>
           
+          <h4>Gold Sponsors</h4>
+          <section id='gold-sponsors'>
+            <div className='card'>
+              <h6>FileCoin</h6>
+              <img src='#' alt='filecoin-logo'/>
+            </div>
+          </section>
         </div>
     </div>
   )
