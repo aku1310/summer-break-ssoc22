066aaea90a6e4bc2d4e7f7151118db8a3c9e8255
Akanksha Kushwaha <cmakanksha@gmail.com>
2022-07-24 18:44:05 +0530

    added timeline prototype

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,12 +1,48 @@
-import React from 'react'
+import React from 'react';
+import '../App.css';
 
 function Timeline() {
   return (
-    <div id='timeline-page' className='page'>
-        Timeline
-        <div id='timeline-container'>
-
+    <div id='timeline-page' className='timeline page'>
+      <div className="timeline-heading">
+        <h1>Timeline</h1>
+      </div>
+      <div className="container left">
+        <div className="date">Date</div>
+        <div className="content">
+          <h2>Heading</h2>
+          <p>
+            Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
+          </p>
         </div>
+      </div>
+      <div className="container right">
+        <div className="date">Date</div>
+        <div className="content">
+          <h2>Heading</h2>
+          <p>
+            Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
+          </p>
+        </div>
+      </div>
+      <div className="container left">
+        <div className="date">Date</div>
+        <div className="content">
+          <h2>Heading</h2>
+          <p>
+            Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
+          </p>
+        </div>
+      </div>
+      <div className="container right">
+        <div className="date">Date</div>
+        <div className="content">
+          <h2>Heading</h2>
+          <p>
+            Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
