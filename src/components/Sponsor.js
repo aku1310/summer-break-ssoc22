756b162f1408cf32e6822bf6b9274cfd75173fd0
Vishrut Aggarwal <vishrut.agg@gmail.com>
2022-07-24 18:53:46 +0530

    Update: Added object for sponsors

--- src/components/Sponsor.js
+++ src/components/Sponsor.js
@@ -1,11 +1,30 @@
 import React from 'react'
 
+let sponsorList = {
+  platinum: [
+    {
+      name: "Devfolio",
+      url: "#"
+    },
+    {
+      name: "Polygon",
+      url: "#"
+    }
+  ],
+  gold: [
+    {
+      name: "FileCoin",
+      url: "#"
+    }
+  ]
+}
+
 function Sponsor() {
   return (
     <div id='sponsor-page' className='page'>
         <h1>Sponsors</h1>
         <div className='sponsor-card-container'>
-
+          
         </div>
     </div>
   )
