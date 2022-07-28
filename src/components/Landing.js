7d32d163431e0952949133d8af7b9d478ae59f50
Vishrut Aggarwal <vishrut.agg@gmail.com>
2022-07-28 19:46:58 +0530

    Fix: add correct link for images and illustrations

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -18,7 +18,7 @@ function Landing() {
         <div id='Details'>
             <p className='accented' id='landing-title'>Summer Break</p><br/><br/>
             <p className='details-landing'>Date: <span className='accented'>August 27, 2022</span></p><br/>
-            <p className='details-landing'>Join us at: <span className='accented'>Bennet University, Greater Noida</span> <a href='https://goo.gl/maps/d6YpcNqU8sDLmhSj6' target='_blank' rel='noreferrer'><img src='https://cdn.discordapp.com/attachments/979642815648772106/1000426067338723378/unknown.png' id='bennet-link-image'/></a></p><br/><br/><br/>
+            <p className='details-landing'>Join us at: <span className='accented'>Bennet University, Greater Noida</span> <a href='https://goo.gl/maps/d6YpcNqU8sDLmhSj6' target='_blank' rel='noreferrer'><img src={require('./../images/outside-link')} id='bennet-link-image' alt='outlink'/></a></p><br/><br/><br/>
             <div 
               className='apply-button' 
               data-hackathon-slug="YOUR-HACKATHON-SLUG" 
@@ -27,7 +27,7 @@ function Landing() {
             ></div>
         </div>
         <div id='illustration-container'>
-          <img src='https://cdn.discordapp.com/attachments/790633998019330148/1000794783381475429/2340666-full-removebg-preview.png' alt='blockchain-illustration'/>
+          <img src={require('./../images/blockchain.png')} alt='blockchain-illustration'/>
         </div>
     </div>
   )
---
+++ src/components/TimelineComingSoon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+
+function Timeline() {
+  return (
+    <div id='timeline-coming-soon-page' className='page'>
+      <div className="timeline-coming-soon-heading">
+        <h1 className='title'>Timeline</h1>
+        <p id='coming-soon'>COMING SOON.... Stay Tuned!!!!</p>
+      </div>
+    </div>
+  )
+}
+
+export default Timeline
