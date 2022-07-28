50950e7fcf97a92ae90acc4a47051fba13e05876
Vishrut Aggarwal <vishrut.agg@gmail.com>
2022-07-28 19:46:18 +0530

    Style: add ssoc logo and coming soon style

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <div id='header'>
-        <img src='' alt='SSOC Logo'/>
+        <img src={require('./../images/social-logo.png')} id='ssoc-logo' alt='SSOC Logo'/>
         <p>in collaboration with CodingMinutes</p>
         <img src='' alt='Coding Minutes Logo'/>
     </div>
