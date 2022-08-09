91265e3535e385f4effdfda8031d8cb2ed0a2bf2
Akanksha Kushwaha <cmakanksha@gmail.com>
2022-08-09 06:22:37 +0530

    Feat: fix timeline styling

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -8,7 +8,11 @@ function Timeline() {
         <h1 className='title'>Timeline</h1>
       </div>
       <div className="container left">
-        <div className="date">10:00 AM - 11:00 AM</div>
+        <div className="date">
+          10:00 AM 
+          - 
+          11:00 AM
+          </div>
         <div className="content">
           <h2>Check in Progress</h2>
           {/* <p>
@@ -17,43 +21,71 @@ function Timeline() {
         </div>
       </div>
       <div className="container right">
-        <div className="date">11:10 AM - 11:30 AM</div>
+        <div className="date">
+          11:10 AM 
+          - 
+          11:30 AM
+          </div>
         <div className="content">
           <h2>Welcome</h2>
         </div>
       </div>
       <div className="container left">
-        <div className="date">11:30 AM - 1:00 PM</div>
+        <div className="date">
+          11:30 AM 
+          - 
+          1:00 PM
+          </div>
         <div className="content">
           <h2>Mohit Workshop</h2>
         </div>
       </div>
       <div className="container right">
-        <div className="date">1:00 PM - 1:20 PM</div>
+        <div className="date">
+          1:00 PM 
+          - 
+          1:20 PM
+          </div>
         <div className="content">
           <h2>Break + Team Formation for Competition</h2>
         </div>
       </div>
       <div className="container left">
-        <div className="date">1:20 PM - 2:00 PM</div>
+        <div className="date">
+          1:20 PM 
+          - 
+          2:00 PM
+          </div>
         <div className="content">
           <h2>AMA with Prateek</h2>
         </div>
       </div>
       <div className="container right">
-        <div className="date">2:00 PM - 3:30 PM</div>
+        <div className="date">
+          2:00 PM 
+          - 
+          3:30 PM
+          </div>
         <div className="content">
           <h2>Short Competition related to Web3 or Other Current Technology</h2>
         </div>
       </div>
       <div className="container left">
-        <div className="date">3:30 PM - 4:00 PM</div>
+        <div className="date">
+          3:30 PM 
+          - 
+          4:00 PM
+          </div>
         <div className="content">
           <h2>Judging + Hangout Session</h2>
         </div>
       </div>
       <div className="container right">
-        <div className="date">4:00 PM - 4:30 PM</div>
+        <div className="date">
+          4:00 PM 
+          - 
+          4:30 PM
+          </div>
         <div className="content">
           <h2>Winner Announcement & Distribution of Certificates & Swags</h2>
         </div>
